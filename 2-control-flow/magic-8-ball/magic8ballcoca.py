3af0bd215fa4961dc5408644abf16ce9338daa42
scar3crowblitz <prahladdonny@gmail.com>
2025-03-15 20:44:18 -0400

    Create magic8ballcoca.py
    optional challenge included

---
+++ 2-control-flow/magic-8-ball/magic8ballcoca.py
@@ -0,0 +1,44 @@
+#This is a program to play Magic 8-ball
+import random
+
+name = ""
+question = "why?"
+answer = ""
+
+random_number = random.randint(1,11)
+
+if random_number == 1:
+  answer = "Yes - definitely"
+elif random_number == 2:
+  answer = ("It is decidely so")
+elif random_number == 3:
+  answer = ("Without a doubt")
+elif random_number == 4:
+  answer = ("Reply hazy, try again")
+elif random_number == 5:
+  answer = ("Ask again later")
+elif random_number == 6:
+  answer = ("Better not tell you now")
+elif random_number == 7:
+  answer = ("My sources say no")
+elif random_number == 8:
+ answer = ("Outlook not so good")
+elif random_number == 9:
+  answer = ("Very doubtful")
+elif random_number == 10:
+  answer = ("better luck next time")
+elif random_number == 11:
+  answer = "keep dreaming"
+else:
+  answer = ("Error")
+
+if question == "" :
+  print("please enter a fckn question")
+
+elif name == "" :
+  print("Question: " + question)
+  print(("Magic 8-ball's answer: ") + answer)
+elif name == "hi":
+  print(name + " asks: " + question)
+  print(("Magic 8-ball's answer: ") + answer)
+
